671e1d7e2c6e1a6123a04fbf5865eb910bf36931
Emlyn Clay <eclay@xyme.ai>
2025-09-19 16:59:14 +0100

    ENG-225: feat: Update the dependencies to the latest numpy and version of prody

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,9 +11,9 @@ INPUTS_DIR = REPO_DIR / "inputs"
 @pytest.fixture(scope="session")
 def protein_mpnn_seed_111_1bc8_backbone() -> str:
     with open(FIXTURE_DIR / "protein_mpnn_seed-111_1BC8/backbones/1BC8_1.pdb") as f:
-        return f.read()
+        return f.read().strip()
 
 @pytest.fixture(scope="session")
 def protein_mpnn_seed_111_1bc8_seq() -> str:
     with open(FIXTURE_DIR / "protein_mpnn_seed-111_1BC8/seqs/1BC8.fa") as f:
-        return f.read()
+        return f.read().strip()
--- tests/ligandmpnn/test_protein_mpnn.py
+++ tests/ligandmpnn/test_protein_mpnn.py
@@ -7,21 +7,23 @@ from tests.conftest import OUTPUTS_DIR, INPUTS_DIR
 
 def test_protein_mpnn_output_unchanged(protein_mpnn_seed_111_1bc8_backbone, protein_mpnn_seed_111_1bc8_seq):
     test_outputs_dir = OUTPUTS_DIR / 'test_protein_mpnn_output_unchanged'
-    
+
     if test_outputs_dir.exists():
         shutil.rmtree(test_outputs_dir)
 
-    subprocess.run(
-        f"""{sys.executable} -m ligandmpnn --seed 111 --model_type protein_mpnn --pdb_path "{str(INPUTS_DIR)}/1BC8.pdb" --out_folder "{str(test_outputs_dir)}" """,
-        shell=True, capture_output=True
-    )
+    cmd = f"""{sys.executable} -m ligandmpnn --seed 111 --model_type protein_mpnn --pdb_path "{str(INPUTS_DIR)}/1BC8.pdb" --out_folder "{str(test_outputs_dir)}" """
+    output = subprocess.run(cmd, shell=True, capture_output=True)
+
+    assert output.returncode == 0, f"""Command: {cmd}
+    Output:
+    {output.stderr}"""
 
     # compare the outputs
     with open(test_outputs_dir / "backbones/1BC8_1.pdb") as f:
-        backbone = f.read()
-        
+        backbone = f.read().strip()
+
     with open(test_outputs_dir / "seqs/1BC8.fa") as f:
-        seq = f.read()
-        
+        seq = f.read().strip()
+
     assert protein_mpnn_seed_111_1bc8_backbone == backbone
     assert protein_mpnn_seed_111_1bc8_seq == seq
