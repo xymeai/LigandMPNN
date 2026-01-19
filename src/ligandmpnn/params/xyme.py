fd3d6d9325f657820a060213f06a25548c5ec5ee
Emlyn Clay <eclay@xyme.ai>
2026-01-19 12:00:42 +0000

    ENG-297: feat: Update the download_models function to make use of Xyme Lake so that it works with our standard environment variables

--- src/ligandmpnn/params/xyme.py
+++ src/ligandmpnn/params/xyme.py
@@ -1,31 +1,27 @@
-import multiprocessing
-import os
 from pathlib import Path
 
-import boto3
 from joblib import Parallel, delayed
+from xyme_lake.lake import XymeDataLake
 
 from ligandmpnn.logger import logger
 from ligandmpnn.params.common import parameter_file_uris
 from ligandmpnn.paths import MODEL_PARAMS_DIR
 
-LIGANDMPNN_MODEL_PARAMS_BUCKET = os.environ.get(
-    "LIGANDMPNN_MODEL_PARAMS_BUCKET", "xyme-data-dev"
-)
+LAKE_MODEL_DIR = "s3://arn:aws:s3:eu-west-2:418272779708:accesspoint/dev-protein-science-access-point/public/protein-science/lmpnn_models"
 
 
-def download_single_s3_file(object_key: str, dest: Path):
-    s3 = boto3.client("s3")
-
+def download_single_s3_file(lake: XymeDataLake, key: str, dest: Path):
     if dest.exists():
         logger.debug(f"Skipping existing file: {dest}")
         return
 
-    s3.download_file(
-        LIGANDMPNN_MODEL_PARAMS_BUCKET,
-        f"protein-science/LigandMPNN_models/{object_key}",
+    lake.s3fs.get(
+        lake.build_s3_uri(
+            team_area="public", team="protein-science", key=f"lmpnn_models/{key}"
+        ),
         str(dest),
     )
+
     logger.debug(f"Downloaded: {dest}")
 
 
@@ -38,10 +34,11 @@ def download_model_parameters(model_parameter_dir: Path = MODEL_PARAMS_DIR) -> N
         return model_parameter_dir / object_key
 
     object_keys = [Path(uri).name for uri in parameter_file_uris]
+    lake = XymeDataLake()
 
     logger.info(f"Checking models from S3...")
-    Parallel(n_jobs=multiprocessing.cpu_count() * 2, prefer="threads")(
-        delayed(download_single_s3_file)(key, destination_path(key))
+    Parallel(n_jobs=2, prefer="threads")(
+        delayed(download_single_s3_file)(lake=lake, key=key, dest=destination_path(key))
         for key in object_keys
     )
     logger.info(f"Models downloaded: {len(object_keys)}")
